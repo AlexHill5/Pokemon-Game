16635d391fb4bd00ae8778a84317460ee5089d2b
Alex Hill <big_al_hill@comcast.net>
2017-08-10 11:09:52 -0600

    pokedex styling and pokedata

--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -7,7 +7,8 @@ class Pokedex extends Component {
         super()
         this.state={
             pokemons: [],
-            pokemon: null
+            pokemon: null,
+            
         }
     }
 
@@ -23,11 +24,14 @@ class Pokedex extends Component {
 
 
     handlePokemonData(i){
+        this.setState({
+            loading: true
+        })
         axios.get(`http://pokeapi.co/api/v2/pokemon/${i}`)
         .then(res=>{
             console.log(res)
             this.setState({
-                pokemon: res
+                pokemon: res,
             })
             console.log('STATE', this.state.pokemon)
         })
@@ -56,7 +60,13 @@ class Pokedex extends Component {
                 </div>
 
                 <div className='info'>
+                    <img  className='who-dat' src={require('./whos-that-pokemon.png')}/>
+                        <div className='info'>
+                        <div className='content-wrapper'>
+                            <img  className='ball2' src={require('./pokeball.png')}/>
 
+                        </div>
+                    </div>
                 </div>
             </div>
         );
@@ -70,11 +80,24 @@ class Pokedex extends Component {
                     </ul>
                 </div>
 
-                <div className='info'>
-                    <div className='content-wrapper'>
-                        <h1 className='detailed-name'> {this.state.pokemon.data.name.toUpperCase()}</h1>
-                    <img className='pokemon-img' src={this.state.pokemon.data.sprites.front_default} />
+                <div className='info1'>
+                    {/*<img  className='who-dat' src={require('./whos-that-pokemon.png')}/>*/}
 
+                    <div className='content-wrapper'>
+                        <div className='stats-wrapper'>
+                            <h1 className='detailed-name'> {this.state.pokemon.data.name.toUpperCase()}</h1>
+                            <ul>
+                                <li className='stat-li'><span className='left'>{this.state.pokemon.data.stats["5"].stat.name.toUpperCase()}</span> <span className='right'>{this.state.pokemon.data.stats["5"].base_stat}</span></li>
+                                <li className='stat-li'><span className='left'>{this.state.pokemon.data.stats["4"].stat.name.toUpperCase()}</span> <span className='right'>{this.state.pokemon.data.stats["4"].base_stat}</span></li>
+                                <li className='stat-li'><span className='left'>{this.state.pokemon.data.stats["3"].stat.name.toUpperCase()}</span> <span className='right'>{this.state.pokemon.data.stats["3"].base_stat}</span></li>
+                                <li className='stat-li'><span className='left'>{this.state.pokemon.data.stats["0"].stat.name.toUpperCase()}</span> <span className='right'>{this.state.pokemon.data.stats["0"].base_stat}</span></li>
+
+                            </ul>
+                        </div>
+                        <div>    
+                            
+                            <img className='pokemon-img' src={this.state.pokemon.data.sprites.front_default} />
+                        </div>
                     </div>
 
 
